0b23548a55da6100c24407a9ef1d47f7f44990e2
Nandhagopal Chellamuthu <dhanasekaran23298@gmail.com>
2022-12-11 17:45:28 +0530

    Sidebar Changes

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Sidebar.css'
-import { Link } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
+
+const initialBackgroundColor = {
+    home: "#d1b726",
+    cities: "#472978"
+};
 
 const Sidebar = () => {
+    const [backgroundColor, setBackgroundColor] = useState(initialBackgroundColor);
+    const location = useLocation();
+
+    useEffect(() => {
+        if(location.pathname === "/") {
+            return setBackgroundColor(initialBackgroundColor);
+        }
+        if(location.pathname === "/cities") {
+            return setBackgroundColor({
+                home: "#472978",
+                cities: "#d1b726"
+            })
+        }
+    }, [location])
     return (
         <div className="sidebar">
-                <ul className="side-list">
-                    <li><Link to="/" className="link">Home</Link></li>
-                    <li><Link to="/cities" className="link" >Cities</Link></li>
-                </ul>
+                <Link to="/" className="link" style={{backgroundColor: backgroundColor.home}}>Home</Link> 
+                <Link to="/cities" className="link" style={{backgroundColor: backgroundColor.cities}}>Cities</Link>
         </div>
     )
 
